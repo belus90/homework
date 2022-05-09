cbbb20355f748eaf01fde123c1bfd850195bba57
belus90 <belus90@gmail.com>
2022-05-09 15:42:28 +0100

    first commit in IntelliJ

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Before
+    public void before(){
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void hasAdd(){
+        assertEquals(25,calculator.add(10, 15));
+    }
+
+
+}
