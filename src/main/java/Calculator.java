807446741f05fa4c309b55ce27290135dcd73140
belus90 <belus90@gmail.com>
2022-05-09 15:55:57 +0100

    Finishing the functions for the calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,8 +1,12 @@
 public class Calculator {
 
-
-
     public int add( int number1, int number2) {
         return number1 + number2;
+    }
+    public int substract(int number1, int number2) {return number1 - number2;}
+
+    public int multiply(int number1, int number2) { return number1 * number2;}
+
+    public double divide(double number1, double number2) {return number1 / number2;
     }
 }
